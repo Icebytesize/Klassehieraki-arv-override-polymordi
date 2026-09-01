50b84b8ceb744c1dbadaa0f5d6880e1ab1bec325
IceByteSize <Matthiasuk@gmail.com>
2026-09-01 09:07:31 +0200

    Add project files.

---
+++ Klassehieraki-arv-override-polymordi/Classes/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klassehieraki_arv_override_polymordi.Classes
+{
+    internal class Employee
+    {
+        private string name;
+        private int employeeId;
+        private DateTime hireDate;
+
+        public Employee(string name, int employeeId, DateTime hireDate)
+        {
+            this.name = name;
+            this.employeeId = employeeId;
+            this.hireDate = hireDate;
+        }
+
+        public virtual decimal CalculateSalary()
+        {
+            // Base salary calculation logic for a generic employee
+            return 30000.00m; // Example base salary
+        }
+
+        public virtual string Description()
+        {
+            return $"{name} tjener {CalculateSalary():C} om måneden"; 
+        }
+    }
+}
